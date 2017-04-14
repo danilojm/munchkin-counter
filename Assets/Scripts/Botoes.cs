55f89df721a44004a47841b7cab7caed4fbf1f91
Danilo Mendes <ejonhson@gmail.com>
2017-04-14 15:54:06 -0300

    first commit

---
+++ Assets/Scripts/Botoes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Botoes : MonoBehaviour {
+
+	[SerializeField]
+	private Text placar = null;
+
+	[SerializeField]
+	private Text total = null;
+
+	private int counterNivel = 0;
+	private string totalCounter;
+
+	public void btnMenos(){
+		counterNivel = counterNivel - 1;
+		placar.text = counterNivel.ToString();
+
+
+		totalCounter = total.text;
+		int totalC = int.Parse (totalCounter);
+		totalC = totalC - 1;
+		total.text = totalC.ToString();
+	}
+
+	public void btnMais(){
+		counterNivel = counterNivel + 1;
+		placar.text = counterNivel.ToString();
+
+		totalCounter = total.text;
+		int totalC = int.Parse (totalCounter);
+		totalC = totalC + 1;
+		total.text = totalC.ToString();
+	}
+}
---
+++ Assets/Scripts/Cliente.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.IO;
+using UnityEngine.UI;
+using System;
+
+public class Cliente : MonoBehaviour {
+
+	public GameObject chatContainer;
+	public GameObject messagePrefab; 
+	public string clientName;
+
+	private bool socketRead;
+	private TcpClient socket;
+	private NetworkStream stream;
+	private StreamWriter write;
+	private StreamReader reader;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (socketRead) {
+			if (stream.DataAvailable) {
+				string data = reader.ReadLine ();
+				if (data != null) {
+					OnIncomingData (data);
+				}
+			}
+		}
+		
+	}
+
+	private void OnIncomingData(string data){
+	
+		if (data == "%NAME") {
+
+			Send("&NAME|" + clientName);
+			return;
+		}
+
+		GameObject go = Instantiate (messagePrefab, chatContainer.transform) as GameObject;
+		go.GetComponentInChildren<Text> ().text = data;
+
+	}
+
+	private void Send(string data){
+	
+		if (!socketRead)
+			return;
+
+		write.WriteLine (data);
+		write.Flush ();
+
+	}
+
+	public void OnSendButton(){
+		string message = GameObject.Find ("SendInput").GetComponent<InputField> ().text;
+		Send (message);
+	}
+
+
+	public void ConnectToServer()
+	{
+		//If already connected, ignore this function
+		if (socketRead)
+			return;
+
+		//Default host / port values
+		string host = "127.0.0.1";
+		int port = 6321;
+
+		string h;
+		int p;
+		h = GameObject.Find ("HostInput").GetComponent<InputField> ().text;
+		if (h != "")
+			host = h;
+		int.TryParse (GameObject.Find ("PortInput").GetComponent<InputField> ().text, out p);
+		if (p != 0)
+			port = p;
+
+		//Create the socket
+
+		try {
+			socket = new TcpClient (host, port);
+			stream = socket.GetStream ();
+			write = new StreamWriter (stream);
+			reader = new StreamReader (stream);
+			socketRead = true;
+		} catch (Exception e) {
+			Debug.Log ("Socket Error: " + e.Message);
+		}
+	}
+
+	private void CloseSocket(){
+		if (!socketRead)
+			return;
+
+		write.Close ();
+		reader.Close ();
+		socket.Close ();
+		socketRead = false;
+	}
+
+	private void OnApplicationQuit(){
+		CloseSocket ();
+	}
+
+	private void OnDisable(){
+		CloseSocket ();
+	}
+}
